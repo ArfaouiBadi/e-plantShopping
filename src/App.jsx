34b10f2eb9781586eff107fb317c702cf71560ca
arfaouibadi <arfaouibadi19@gmail.com>
2024-09-07 09:42:18 -0400

    finished productList component

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           <p>Where Green Meets Serenity</p>
          
           <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
+            Get Startedd
           </button>
          </div>
           <div className="aboutus_container">
